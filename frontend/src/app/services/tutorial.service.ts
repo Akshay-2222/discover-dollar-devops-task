3dde3864f22b4568ed537c2e718909af236288af
Akshay <s.akshaykiumar2222@gmail.com>
2026-02-24 21:46:10 +0530

    Fix: Move baseUrl inside TutorialService class

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,40 +3,40 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
+const baseUrl = '/api/tutorials';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
-  // baseUrl must be inside the class
-  private baseUrl = '/api/tutorials';
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(this.baseUrl);
+    return this.http.get<Tutorial[]>(baseUrl);
   }
 
   get(id: any): Observable<Tutorial> {
-    return this.http.get<Tutorial>(`${this.baseUrl}/${id}`);
+    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(this.baseUrl, data);
+    return this.http.post(baseUrl, data);
   }
 
   update(id: any, data: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}/${id}`, data);
+    return this.http.put(`${baseUrl}/${id}`, data);
   }
 
   delete(id: any): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${baseUrl}/${id}`);
   }
 
   deleteAll(): Observable<any> {
-    return this.http.delete(this.baseUrl);
+    return this.http.delete(baseUrl);
   }
 
   findByTitle(title: any): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(`${this.baseUrl}?title=${title}`);
+    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
   }
 }
